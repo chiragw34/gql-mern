a083cd85d22b6811dfd7863284488c837a3ba72f
chirag <chiragw34@gmail.com>
2019-12-23 18:45:57 +0530

    added posts on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,62 @@
 import React from "react";
+import { Grid, Image } from "semantic-ui-react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import PostCard from "../components/PostCard";
 
 function Home() {
+  let posts = ''
+  const FETCH_POSTS_QUERY = gql`
+    {
+      getPosts {
+        id
+        body
+        createdAt
+        username
+        likeCount
+        likes {
+          username
+        }
+        commentCount
+        comments {
+          id
+          username
+          body
+          createdAt
+        }
+      }
+    }
+  `;
+
+  const {
+    loading,
+    data
+  } = useQuery(FETCH_POSTS_QUERY);
+
+  if(data){
+    posts = {data: data.getPosts }
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <Grid columns={3}>
+        <Grid.Row className='page-title'>
+          <h1>Recent Posts</h1>
+        </Grid.Row>
+        <Grid.Row>
+          {loading ? (
+            <h1>LOADING POSTS...</h1>
+          ) : (
+            posts.data &&
+            posts.data.map(post => (
+              <Grid.Column key={post.id} style={{marginBottom:20}}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))
+          )}
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
