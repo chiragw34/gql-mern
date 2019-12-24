771eaad093f0af9c04e3536cdd038d307d489be8
Chirag Wadhwa <chiragw34@gmail.com>
2019-12-25 00:46:17 +0530

    posts can be added in realtime

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,56 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Grid } from "semantic-ui-react";
 import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
 
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from '../components/PostForm'
+import {FETCH_POSTS_QUERY} from '../util/graphql'
 
 function Home() {
-  let posts = ''
-  const FETCH_POSTS_QUERY = gql`
-    {
-      getPosts {
-        id
-        body
-        createdAt
-        username
-        likeCount
-        likes {
-          username
-        }
-        commentCount
-        comments {
-          id
-          username
-          body
-          createdAt
-        }
-      }
-    }
-  `;
+  const { user } = useContext(AuthContext);
+  let posts = "";
+  
 
-  const {
-    loading,
-    data
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
 
-  if(data){
-    posts = {data: data.getPosts }
+  if (data) {
+    posts = { data: data.getPosts };
   }
 
   return (
     <div>
       <Grid columns={3}>
-        <Grid.Row className='page-title'>
+        <Grid.Row className="page-title">
           <h1>Recent Posts</h1>
         </Grid.Row>
         <Grid.Row>
+          {user && (
+            <Grid.Column>
+              <PostForm />
+            </Grid.Column>
+          )}
           {loading ? (
             <h1>LOADING POSTS...</h1>
           ) : (
             posts.data &&
             posts.data.map(post => (
-              <Grid.Column key={post.id} style={{marginBottom:20}}>
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
                 <PostCard post={post} />
               </Grid.Column>
             ))
